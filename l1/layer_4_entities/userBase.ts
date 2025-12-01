83efc6ca7448634382337e7b5b71d49ff15ab459
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2025-12-01 10:06:22 -0300

    Teste auditoria

--- l1/layer_4_entities/userBase.ts
+++ l1/layer_4_entities/userBase.ts
@@ -1,11 +1,19 @@
 /// <mls shortName="userBase" project="102021" folder="layer_4_entities" enhancement="_blank" groupName="layer_4_entities" />
 
-import { UserRecord } from "./user.js";    
+import { UserRecord, UserIndexRecord } from "./user.js";    
 
 export interface UserBase { 
     upd: (param: UserRecord) => Promise<UserRecord>;
     add: (param: UserRecord) => Promise<UserRecord>;
     del: (id: number) => Promise<boolean>;
     list: () => Promise<UserRecord[]>;
+    
+}
+
+export interface UserIndexBase { 
+    upd: (param: UserIndexRecord) => Promise<UserIndexRecord>;
+    add: (param: UserIndexRecord) => Promise<UserIndexRecord>;
+    del: (id: number) => Promise<boolean>;
+    list: (param:string) => Promise<UserIndexRecord[]>;
     
 }
