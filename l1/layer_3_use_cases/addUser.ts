607cb26bc063e94a96985d0b1e3aeac78e83a402
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2025-12-01 10:06:16 -0300

    Teste auditoria

--- l1/layer_3_use_cases/addUser.ts
+++ l1/layer_3_use_cases/addUser.ts
@@ -1,10 +1,26 @@
 /// <mls shortName="addUser" project="102021" folder="layer_3_use_cases" enhancement="_blank" groupName="layer_3_use_cases" />
 
 import { UserRecord } from "../layer_4_entities/user.js";
+import { AuditRecord } from "../layer_4_entities/audit.js"; 
 import { Ctx } from "../common/local.js"; 
 
 export async function addUser(ctx:Ctx, data: UserRecord): Promise<UserRecord> {
 
-    return await ctx.io.user.add(data);
+    const reg =  await ctx.io.local.user.add(data);
+
+    const regIndex = await ctx.io.dynamoDb.user.add({id:reg.id as number, fullText: `${reg.details.name}|${reg.details.cpf}` })
+    
+    const audit: AuditRecord = {
+        user:  "system",  
+        date: new Date().toISOString(),
+        action: "add",
+        origin: "User",
+        description: `User create: ${reg.id }`
+    };
+
+    // 3. Salva o registro de auditoria
+    ctx.io.local.audit.add(audit);
+
+    return reg;
 
 }
