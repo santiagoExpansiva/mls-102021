ea85ff3f0c1f485ea3b6b34bc97e00d9239b8234
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2025-12-09 14:46:47 -0300

    refactor .test.ts imports

--- l2/agentEndpointLayer2Controller.test.ts
+++ l2/agentEndpointLayer2Controller.test.ts
@@ -1,5 +1,5 @@
 /// <mls shortName="agentEndpointLayer2Controller" project="102021" enhancement="_blank" folder="" />
 
- import { ICANTest, ICANIntegration, ICANSchema  } from './_100554_tsTestAST';
+ import { ICANTest, ICANIntegration, ICANSchema  } from '/_100554_/l2/tsTestAST.js';
  export const integrations: ICANIntegration[] = [];
  export const tests: ICANTest[] = [];
