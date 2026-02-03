f615a3fe37d6ed410e3786fc1d383d174872ab34
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-03 08:31:19 -0300

    save: l2/agentEndpointLayer1localDB.ts

--- l2/agentEndpointLayer1localDB.ts
+++ l2/agentEndpointLayer1localDB.ts
@@ -1,4 +1,4 @@
-/// <mls shortName="agentEndpointLayer1localDB" project="102021" enhancement="_blank" />
+/// <mls fileReference="_102021_/l2/agentEndpointLayer1localDB.ts" enhancement="_blank" />
 
 import { IAgent, svg_agent } from '/_100554_/l2/aiAgentBase.js';
 import { getPromptByHtml } from '/_100554_/l2/aiPrompts.js';
