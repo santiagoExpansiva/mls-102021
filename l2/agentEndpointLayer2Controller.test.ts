66b6dab4d7a74cfbd952e04df3e8022eb565dc56
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-03 08:31:24 -0300

    save: l2/agentEndpointLayer2Controller.test.ts

--- l2/agentEndpointLayer2Controller.test.ts
+++ l2/agentEndpointLayer2Controller.test.ts
@@ -1,4 +1,4 @@
-/// <mls shortName="agentEndpointLayer2Controller" project="102021" enhancement="_blank" folder="" />
+/// <mls fileReference="_102021_/l2/agentEndpointLayer2Controller.test.ts" enhancement="_blank" />
 
  import { ICANTest, ICANIntegration, ICANSchema  } from '/_100554_/l2/tsTestAST.js';
  export const integrations: ICANIntegration[] = [];
