93b903982ca6e204a4bc3d4098de90545473be21
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-02-03 08:31:21 -0300

    save: l2/agentEndpointLayer2Controller.defs.ts

--- l2/agentEndpointLayer2Controller.defs.ts
+++ l2/agentEndpointLayer2Controller.defs.ts
@@ -1,2 +1,2 @@
-/// <mls shortName="agentEndpointLayer2Controller" project="102021" enhancement="_blank" folder="" />
+/// <mls fileReference="_102021_/l2/agentEndpointLayer2Controller.defs.ts" enhancement="_blank" />
 
