db6849c312e54543137f2ec130b344526503ac85
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-06-25 14:46:39 -0300

    Update project.ts

--- l2/project.ts
+++ l2/project.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_102021_/l2/project.ts" enhancement="_blank" />
+/// <mls fileReference="_102021_/l2/project.ts" enhancement="_blank" /> 
  
 export const projectConfig = {
   "modules": [], 
